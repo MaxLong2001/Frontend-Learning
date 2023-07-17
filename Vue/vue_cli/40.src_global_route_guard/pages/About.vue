1320c794ddda61cbd51ebc45971ecce956ee184d
MaxLong <longyizhou2001@163.com>
2023-07-18 00:12:53 +0800

    2023.7.17

---
+++ Vue/vue_cli/40.src_global_route_guard/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>This is About.</h2>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  mounted() {
+    console.log('About mounted!')
+  },
+  beforeDestroy() {
+    console.log('About beforeDestroy!')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/vue_cli/41.src_exclusive_route_guard/pages/Detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li>Message id: {{ id }}</li>
+    <li>Message title: {{ title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  props: ['id', 'title']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
