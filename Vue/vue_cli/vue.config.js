21b59d10999a7bb074dc0893d75001acedf776c2
MaxLong <longyizhou2001@163.com>
2023-07-18 20:12:25 +0800

    vue_cli

--- Vue/vue_cli/vue.config.js
+++ Vue/vue_cli/vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    // devServer: {
+    //   proxy: 'http://localhost:5000'
+    // }
+
+    devServer: {
+        proxy: {
+            '/maxlong': {
+                target: 'http://localhost:5000',
+                pathRewrite: {'^/maxlong': ''},
+                ws: true, // proxy websockets
+                changeOrigin: true // needed for virtual hosted sites
+            },
+            '/diana': {
+                target: 'http://localhost:5001',
+                pathRewrite: {'^/diana': ''},
+                ws: true, // proxy websockets
+                changeOrigin: true // needed for virtual hosted sites
+            }
+        }
+    }
 })
